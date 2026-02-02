559de9bdbe313f8b758416562e3b97756a8d2af9
Kirtan Patel <kjpatel200022@gmail.com>
2026-02-02 19:33:40 +0530

    feat: Implement initial root layout with comprehensive metadata, Google Analytics, theming, and font configuration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const roboto = Roboto({subsets:['latin'],variable:'--font-sans'});
@@ -36,6 +37,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-K97XZWQ6QB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-K97XZWQ6QB');
+          `}
+        </Script>
+      </head>
       <body
         className={`antialiased ${roboto.variable} custom-scrollbar`}
       >
